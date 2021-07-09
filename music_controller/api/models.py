5dab193068af2fe3119ec59f73d96d9b46e6d65d
Kshitij Kavimandan <kshitijk3188@gmail.com>
2021-07-09 19:08:11 +0530

    Commit

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -17,9 +17,9 @@ def generate_unique_code():
 
 
 class Room(models.Model):
+    id = models.AutoField(primary_key=True)
     code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
-
