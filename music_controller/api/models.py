caa142f41eb72a9ec0069c2047e9cdcabdc7697a
Kshitij Kavimandan <kshitijk3188@gmail.com>
2021-07-10 16:47:29 +0530

    Handling POST requests

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -13,12 +13,10 @@ def generate_unique_code():
 
     return code
 
-# Create your models here.
-
 
 class Room(models.Model):
-    id = models.AutoField(primary_key=True)
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
