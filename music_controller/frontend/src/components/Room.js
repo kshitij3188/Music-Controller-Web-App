ba78acafd863a4427ef24bd79c5aab6b8f0ed9c5
Kshitij Kavimandan <kshitijk3188@gmail.com>
2021-07-15 11:13:43 +0530

    Creating Room

---
+++ music_controller/frontend/src/components/Room.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Room extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votesToSkip: 2,
+            guestCanPause: false,
+            isHost: false,
+        };
+    }
+
+    render() {
+        return <div>
+            <p>{this.state.votesToSkip}</p>
+            <p>Guest Can Pause: {this.state,guestCanPause}</p>
+            <p>Host: {this.state.isHost}</p>
+        </div>
+    }
+}
